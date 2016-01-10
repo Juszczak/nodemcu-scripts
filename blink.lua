5f1231980837962427f5900fa098c9bb8a13ed68
Adrian Juszczak <adrian@juszczak.io>
2016-01-10 23:31:28 +0100

    blink lib now uses led lib

--- blink.lua
+++ blink.lua
@@ -1,23 +1,21 @@
+local led = require("led")
+
 local blink = {}          -- init module
 
-local pin = 4             -- gpio2
-local val = gpio.LOW      -- init low val
 local freq = 1000         -- default freq
 local blink_tmr = 6       -- id for blinking tmr
 
 function blink.toggle()
-	if val == gpio.LOW then -- toggle val
-		val = gpio.HIGH
+	if led.is_on() then
+		led.off()
 	else
-		val = gpio.LOW
+		led.on()
 	end
-	gpio.write(pin, val)    -- write val to gpio2
 end
 
 function blink.start(arg)
 	freq = arg
-	gpio.mode(pin, gpio.OUTPUT)
-	gpio.write(pin, val)
+	led.init()
 	tmr.alarm(blink_tmr, freq, 1, blink.toggle)
 end
 
